c963103c51c3fb7cb694b6ef5b90ffdbf04b1c4b
dvdrtrgn <dvdrtrgn@gmail.com>
2015-09-17 12:13:48 -0500

    reduce debug

--- app/_load.js
+++ app/_load.js
@@ -96,8 +96,8 @@ var Data, Load, Tests, ShareStrings, switchTo5x = true;
         nope: [
             'http://www.wellsfargomedia.com/lib/js/ga-ecg.js',
             /*'http://www.wellsfargomedia.com/lib/video-js/videojs.ga.js',*/
-            G.ven + 'sharethis.lib.js',
-            G.ven + 'sharethis.cfg.js',
+//            G.ven + 'sharethis.lib.js',
+//            G.ven + 'sharethis.cfg.js',
         ],
     };
     M.load([G.Load.base, G.Load.font, G.Load.main, G.Load.test]);
