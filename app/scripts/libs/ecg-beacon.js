70279ead4d542f9f49c905e7303153b2607a5c10
dvdrtrgn <dvdrtrgn@gmail.com>
2015-10-09 10:57:05 -0500

    standardize beacon

--- app/scripts/libs/ecg-beacon.js
+++ app/scripts/libs/ecg-beacon.js
@@ -1,12 +1,11 @@
 /*jslint white:false, -W069, -W009 */
 /*global define, ga, window */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- recreated drt 2015-10
+ recreated drt 2015-10-09
 
  USE
  constructor
- buffer analytics event for every page interaction
- prevent new events for set interval (def:15s)
+ set interval buffer (def:15s) for analytics interaction event
 
  TODO
  document a bit
@@ -18,11 +17,17 @@ define(function () {
     var Api,
         Nom = 'Beacon',
         W = (W && W.window || window),
-        C = (W.C || W.console);
-    var start,
-        goog = '//www.google-analytics.com/analytics.js';
+        C = (W.C || W.console),
+        Df;
 
-    function isogram(i, s, o, g, r, a, m) {
+    Df = {
+        act: 'mousemove',
+        lab: 'interaction',
+        ua: 'UA-5483042-1',
+        goog: '//www.google-analytics.com/analytics.js',
+    };
+
+    function isogram(i, s, o, g, r, a, m) { // from google
         i['GoogleAnalyticsObject'] = r;
         i[r] = i[r] || function () {
             (i[r].q = i[r].q || new Array()).push(arguments);
@@ -35,7 +40,7 @@ define(function () {
         m.parentNode.insertBefore(a, m);
     }
 
-// HELPERS (defaults dependancy only)
+// HELPERS (top dependancy only)
     function _dump() {
         _log(Nom, '(dump)', arguments[1]);
     }
@@ -53,34 +58,22 @@ define(function () {
 // EXPOSED (prototype interface)
     Api = {
         _: Nom,
-        db: null,
-        interval: 15e3, // 15 second intervals
         nom: Nom,
-        act: 'mousemove',
-        lab: 'engagement',
-        getStart: function () {
-            start = (start || _now());
-            return start;
-        },
-        getSpent: function () {
-            return _now() - this.getStart();
-        },
-        throttle: function (func, wait) {
+        db: null,
+        interval: 15,
+        throttle: function (func, wait) { // from lodash
             var args, result, thisArg,
                 previous = _now(),
                 timeoutId = null;
-
             function trailingCall() {
                 previous = _now();
                 timeoutId = null;
                 result = func.apply(thisArg, args);
             }
-
             return function () {
                 var now = _now(),
                     elapsed = now - previous,
                     remaining = wait - elapsed;
-
                 args = arguments;
                 thisArg = this;
 
@@ -99,46 +92,44 @@ define(function () {
             (W.ga ? W.ga : _dump)('send', {
                 hitType: 'event',
                 eventCategory: this.nom,
-                eventAction: this.act,
-                eventLabel: this.lab,
+                eventAction: Df.act,
+                eventLabel: Df.lab,
                 eventValue: this.interval, // seconds > 0
             });
         },
         makeLimitedSend: function () {
             var self = this;
-            _log(this.nom, 'running ' + (W.ga ? 'LIVE' : 'in debug'), this);
+            _log(this.nom, W.ga ? 'running' : 'debugging', this);
 
             return this.throttle(function () {
                 self.sendBeacon();
             }, this.interval * 1000);
         },
         init: function (sec) {
-            start = _now();
             this.db = W.debug > 0;
             this.interval = sec ? sec : this.interval;
 
-            if (!W.ga && !this.db) { // really load analytics?
-                isogram(W, W.document, 'script', goog, 'ga');
-                W.ga('create', 'UA-5483042-1', 'auto');
+            if (!W.ga && !this.db) { // loaded analytics?
+                isogram(W, W.document, 'script', Df.goog, 'ga');
+                W.ga('create', Df.ua, 'auto');
                 W.ga('send', 'pageview');
             }
 
             this.init = W.addEventListener ? // respond to any movement
-                W.document.addEventListener(this.act, this.makeLimitedSend()) :
-                W.document.attachEvent('on' + this.act, this.makeLimitedSend()); // IE
-            delete this.init; // prevent double-bind
+                W.document.addEventListener(Df.act, this.makeLimitedSend()) :
+                W.document.attachEvent('on' + Df.act, this.makeLimitedSend()); // IE
+            delete this.init; // prevent reinit
         },
     };
 
 // CONSTRUCT
     function Beacon(sec, nom) {
+        this.constructor = Beacon;
         if (nom) {
             this.nom = nom;
         }
         this.init(sec, nom);
-        this.constructor = Beacon;
     }
-
     Beacon.prototype = Api;
     return Beacon;
 });
